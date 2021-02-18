477bc7bcc0e0e86b07d63bf11da6472224d5785a
Jarkko Viikki <jarkko.viikki@gmail.com>
2021-02-18 14:59:02 +0200

    Improved project structure (moved files and directories around)
    The main entry point is easier to find now.

--- webpack.config.development.js
+++ webpack.config.development.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   mode: 'development',
-  entry: './src/interpreter/interpreter.ts',
+  entry: './src/main.ts',
   devtool: 'inline-source-map',
   devServer: {
     contentBase: './dist',
